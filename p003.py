be457d32303fd9e952acf04f0e9d40e966e3e592
Andrea Fresco <andreafresco6@gmail.com>
2021-10-11 14:48:27 +0200

    Problem 3 added

--- p003.py
+++ p003.py
@@ -3,21 +3,42 @@
 # Copyright (c) Andrea Fresco. All rights reserved.
 # 
 # https://projecteuler.net/problem=3
-# 
-
-def IsPrime1(num):
+# https://github.com/andreafresco/Project-Euler
+#
     
-   
+def smallest_prime(n): # return the smallest prime of n or n itself
+    # if it is prime
+    
+    assert n > 0
+    
     i = 2
     
-    while i*i <= num:
-        if num%i == 0:
-            return False
-        i += 1
+    # we restrict the search of prime numbers between 2 and sqrt(n)
+    while i*i <= n: # equivalent to i <= sqrt(n)
         
-    return True
+        if n%i == 0:
+            return i
+        
+        i+=1
+        
+    return n
 
+
+def Largest_Prime_Factor(P):
+    
+    smallest = smallest_prime(P) # finding the smallest prime
+    
+    while smallest < P: 
+        
+        # Dividing the number by all it's smallest prime numbers until we reach
+        # the highest prime number (condition: P = smallest)
+        P //= smallest
+        smallest = smallest_prime(P)
+        
+    return P
+    
 
 if __name__ == "__main__":
     
-    print(IsPrime1(5))
+    print(Largest_Prime_Factor(600851475143))
+    
